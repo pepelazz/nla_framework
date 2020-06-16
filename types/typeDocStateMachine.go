18e22f09296312fa8e8ec65873e98925fece9af6
pepelazz <pepelazz00@gmail.com>
2020-06-16 23:22:23 +0300

    modify state machine

--- types/typeDocStateMachine.go
+++ types/typeDocStateMachine.go
@@ -19,7 +19,6 @@ type (
 		To                string
 		Label             string
 		UpdateFlds        []FldType              // поля, которые заполняются при смене стейта
-		CopyToHistoryFlds []FldType              // поля, значения которых копируются в историю изменений при смене статуса
 		Conditions        []DocSmActionCondition // условия выполнения экшена
 		Hooks DocSmActionlHooks
 	}
