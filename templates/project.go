fd369c027e119de07b26437e0111d061d68656ae
pepelazz <pepelazz00@gmail.com>
2020-08-06 22:34:19 +0300

    fix

--- templates/project.go
+++ templates/project.go
@@ -51,6 +51,7 @@ func WriteProjectFiles(p types.ProjectType, tmplMap map[string]*template.Templat
 	ReadTmplAndPrint(p, projectTmplPath + "/webClient/app/components/users/item.vue", "/webClient/src/app/components/users",  "item.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + "/webClient/app/components/currentUser/profile.vue", "/webClient/src/app/components/currentUser",  "profile.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + "/webClient/app/components/home.vue", "/webClient/src/app/components",  "home.vue", nil)
+	ReadTmplAndPrint(p, projectTmplPath + "/webClient/app/components/auth/loginPage.vue", "/webClient/src/app/components/auth",  "loginPage.vue", nil)
 
 	if p.IsTelegramIntegration() {
 		ReadTmplAndPrint(p, "../../../pepelazz/projectGenerator/templates/integrations/telegram/telegramAuth.go", "/webServer", "telegramAuth.go", nil)
--- templates/project/webClient/app/components/auth/loginPage.vue
+++ templates/project/webClient/app/components/auth/loginPage.vue
@@ -2,9 +2,10 @@
   <div class="layout">
     <div>
       <div class="row justify-center" style="margin-top: 20px; margin-bottom: 10px">
-        <img
-          src="[[appLogoSrc]]"
-          alt="" style="width: auto; max-height: 100px">
+        [[if .Config.Logo]]<img src="[[.Config.Logo]]" alt="" style="width: auto; max-height: 100px">
+        [[else]]
+        <h5>[[.Vue.UiAppName]]</h5>
+        [[- end]]
       </div>
       <!--Кнопки авторизации-->
       <div class="row justify-center q-mt-lg">
