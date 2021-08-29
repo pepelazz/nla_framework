fb7f35cf3e96d6e745666a91fc4af05992dfe3bf
pepelazz <pepelazz00@gmail.com>
2021-08-29 13:30:18 +0300

    compDocList.vue change default sort to - created_at desc

--- webClient/quasar_2/webClient/src/app/components/common/list/compDocList.vue
+++ webClient/quasar_2/webClient/src/app/components/common/list/compDocList.vue
@@ -114,7 +114,7 @@
         searchTxt: '',
         isShowSearchfld: false,
         itemList: [],
-        listParams: {page: 0, per_page: 10, deleted: false},
+        listParams: {page: 0, per_page: 10, deleted: false, order_by: 'created_at desc'},
         isUrlQueryProcessed: false, // флаг для обработки query из url при первоначальной загрузке
       }
     },
