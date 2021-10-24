776e04643449257e9e80a6df80566b8a740db4df
pepelazz <pepelazz00@gmail.com>
2021-10-24 13:32:16 +0300

    fis compFldDateTime.vue

--- webClient/quasar_2/webClient/src/app/components/common/list/compFldDateTime.vue
+++ webClient/quasar_2/webClient/src/app/components/common/list/compFldDateTime.vue
@@ -3,7 +3,7 @@
     <template v-slot:prepend  v-if="!readonly">
       <q-icon name="event" class="cursor-pointer">
         <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-          <q-date v-model="date" mask="DD-MM-YYYY" :readonly="readonly">
+          <q-date v-model="date" mask="DD-MM-YYYY HH:mm" :readonly="readonly">
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Ok" color="primary" flat />
             </div>
@@ -26,30 +26,31 @@
 </template>
 
 <script>
-  // import {date as qDate} from 'quasar'
-  import {date as qDate} from 'quasar'
-  export default {
-    props: ['dateString', 'label', 'readonly'],
-    data() {
-      return {
-        date: null,
+import {date as qDate} from 'quasar'
+export default {
+  props: ['dateString', 'label', 'readonly'],
+  data() {
+    return {
+      date: null,
+    }
+  },
+  watch: {
+    date: function (newVal, oldVal) {
+      newVal = newVal?.trim()
+      oldVal = oldVal?.trim()
+      // console.log('newVal:', newVal, 'oldVal:', oldVal, 'newVal !== oldVal:', newVal.trim() !== oldVal, 'length', newVal.length)
+      if (newVal !== oldVal) {
+        let d = qDate.extractDate(newVal, 'DD-MM-YYYY HH:mm')
+        // обрабатываю дату только если между 1900 и 2050 годами
+        if (qDate.isBetweenDates(d, new Date(1900, 1, 1), new Date(2050, 1, 1))) {
+          let res = qDate.formatDate(d, 'YYYY-MM-DDTHH:mm:ss')
+          this.$emit('update', res)
+        }
       }
     },
-    watch: {
-      date: function (newVal, oldVal) {
-        // console.log('newVal:', newVal, 'oldVal:', oldVal, 'newVal !== oldVal:', newVal !== oldVal)
-        if (newVal !== oldVal) {
-          let d = qDate.extractDate(newVal, 'DD-MM-YYYY HH:mm')
-          // обрабатываю дату только если между 1900 и 2050 годами
-          if (qDate.isBetweenDates(d, new Date(1900, 1, 1), new Date(2050, 1, 1))) {
-            let res = qDate.formatDate(d, 'YYYY-MM-DDTHH:mm:ss')
-            this.$emit('update', res)
-          }
-        }
-      },
-    },
-    mounted() {
-      if (this.dateString) this.date = this.dateString
-    }
+  },
+  mounted() {
+    if (this.dateString) this.date = this.dateString
   }
+}
 </script>
