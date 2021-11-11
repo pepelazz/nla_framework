88d2bb78c19edf889ec49cbc7d6fe38ae18791ee
pepelazz <pepelazz00@gmail.com>
2021-11-11 18:12:51 +0300

    add vif in compEditDialog.vue

--- templates/project.go
+++ templates/project.go
@@ -60,6 +60,7 @@ func WriteProjectFiles(p types.ProjectType, tmplMap map[string]*template.Templat
 	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/currentUser/profile.vue", "/webClient/src/app/components/currentUser",  "profile.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/currentUser/messages/list.vue", "/webClient/src/app/components/currentUser/messages",  "list.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/home.vue", "/webClient/src/app/components",  "home.vue", nil)
+	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/auth/index.vue", "/webClient/src/app/components/auth",  "index.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/auth/loginPage.vue", "/webClient/src/app/components/auth",  "loginPage.vue", nil)
 	ReadTmplAndPrint(p, projectTmplPath + webClient + "/app/components/auth/email/components/compRegisterForm.vue", "/webClient/src/app/components/auth/email/components",  "compRegisterForm.vue", nil)
 
