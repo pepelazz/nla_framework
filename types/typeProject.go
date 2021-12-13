e1a77ccf81f62234b75c1e70334f8d9ec3d46f33
pepelazz <pepelazz00@gmail.com>
2021-12-13 14:19:58 +0300

    fix

--- types/typeProject.go
+++ types/typeProject.go
@@ -70,6 +70,7 @@ type (
 		DbName   string
 		Port     int64
 		Password string
+		Host string
 		TimeZone string // время для сервера default 'Europe/Moscow' (например 'Asia/Novosibirsk')
 		Version string // версия Postgres, по дефолту 12
 	}
