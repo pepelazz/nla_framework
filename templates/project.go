3228465160c11892712ac6dbbc4bac8059601e32
pepelazz <pepelazz00@gmail.com>
2022-10-26 15:07:15 +0300

    add tmpl user_temp_email_auth_create

--- templates/project.go
+++ templates/project.go
@@ -49,6 +49,7 @@ func WriteProjectFiles(p types.ProjectType, tmplMap map[string]*template.Templat
 	ReadTmplAndPrint(p, projectTmplPath+"/sql/01_User/main.toml", "/sql/model/01_User", "main.toml", nil)
 	ReadTmplAndPrint(p, projectTmplPath+"/sql/03_UserTempEmailAuth/main.toml", "/sql/model/03_UserTempEmailAuth", "main.toml", nil)
 	ReadTmplAndPrint(p, projectTmplPath+"/sql/03_UserTempEmailAuth/user_temp_email_auth_check_token.sql", "/sql/template/function/_UserTempEmailAuth", "user_temp_email_auth_check_token.sql", nil)
+	ReadTmplAndPrint(p, projectTmplPath+"/sql/03_UserTempEmailAuth/user_temp_email_auth_create.sql", "/sql/template/function/_UserTempEmailAuth", "user_temp_email_auth_create.sql", nil)
 	ReadTmplAndPrint(p, projectTmplPath+"/jobs/main.go", "/jobs", "main.go", nil)
 	ReadTmplAndPrint(p, projectTmplPath+"/pg/pgListener.go", "/pg", "pgListener.go", nil)
 
